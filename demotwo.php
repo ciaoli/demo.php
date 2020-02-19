95ce3ef3046a998a4f9d28edd019f645ac3de22a
ciaociao <misterkome0514@yahoo.co.jp>
2020-02-19 22:34:18 +0000

    laravel3 commit

---
+++ demotwo.php
@@ -0,0 +1,40 @@
+<?php
+
+$name = "Shoya";
+
+if ($name = "Shoya") {
+    echo "私はあなたの名前です。";
+   } else {
+    echo "私はあなたの名前ではありません。";
+   }
+echo "\n";
+
+
+$total = 0;
+
+for ($i = 1; $i <= 100; $i++) {
+    $total += $i;
+}
+echo $total;
+echo "\n";
+
+$fruits = array("apple", "orange", "lemon", "banana", "peach");
+
+foreach($fruits as $fruit) {
+    echo $fruit;
+    echo "\n";
+}
+
+/* for文の始めの値を定義する /
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = 1; $i <= 100; $i++){
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
